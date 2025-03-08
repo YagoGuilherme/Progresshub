9f9fa14bbdf1e688514c506f4f5513479b31b036
Yago <yagogui559@gmail.com>
2025-03-07 21:18:16 -0300

    atualização

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         alert('Usuário ou senha incorretos!');
     }
 });
+
+// Mostrar/esconder senha
+document.getElementById('togglePassword').addEventListener('click', function() {
+    const passwordInput = document.getElementById('password');
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    this.classList.toggle('fa-eye-slash');
+});
